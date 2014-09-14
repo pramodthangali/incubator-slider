28005bf92f7a2904d815358b5d973c097d77807d
Sumit Mohanty <smohanty@hortonworks.com>
2014-09-14 07:25:22 -0700

    SLIDER-81. support placement of containers on labelled YARN nodes

--- slider-core/src/test/groovy/org/apache/slider/providers/agent/TestBuildBasicAgent.groovy
+++ slider-core/src/test/groovy/org/apache/slider/providers/agent/TestBuildBasicAgent.groovy
@@ -354,7 +354,56 @@ class TestBuildBasicAgent extends AgentTestBase {
       fail("Build operation should not fail")
     }
   }
-  
+
+  @Test
+  public void testLabelExpressionArgs() throws Throwable {
+    String clustername = createMiniCluster(
+        "",
+        configuration,
+        1,
+        1,
+        1,
+        true,
+        false)
+
+    try {
+      buildAgentCluster(clustername,
+          [:],
+          [
+              ARG_OPTION, CONTROLLER_URL, "http://localhost",
+              ARG_PACKAGE, ".",
+              ARG_OPTION, APP_DEF, "file://" + appDef.absolutePath,
+              ARG_RESOURCES, TEST_FILES + "good/resources_with_label.json",
+              ARG_TEMPLATE, TEST_FILES + "good/appconf.json"
+          ],
+          true, false,
+          false)
+    } catch (BadConfigException exception) {
+      log.error(
+          "Build operation should not have failed with exception : \n$exception")
+      fail("Build operation should not fail")
+    }
+
+    AggregateConf instanceDefinition = loadInstanceDefinition(clustername)
+    def opt = instanceDefinition.getResourceOperations().getComponentOpt(
+        "node1",
+        ResourceKeys.YARN_LABEL_EXPRESSION,
+        null)
+    assert opt == null, "Expect null"
+
+    opt = instanceDefinition.getResourceOperations().getComponentOpt(
+        "node2",
+        ResourceKeys.YARN_LABEL_EXPRESSION,
+        null)
+    assert opt == "", "Expect empty string"
+
+    opt = instanceDefinition.getResourceOperations().getComponentOpt(
+        "node3",
+        ResourceKeys.YARN_LABEL_EXPRESSION,
+        null)
+    assert opt == "coquelicot && amaranth", "Expect colors you have not heard of"
+  }
+
   @Test
   public void testBadAgentArgs() throws Throwable {
     String clustername = createMiniCluster(
